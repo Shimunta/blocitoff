ba58889f2de8bf0274c9f657dff41ac41144d814
Daniel Lisa <danlisa89@gmail.com>
2016-05-17 11:54:28 -0400

    destroying stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,20 @@ class ItemsController < ApplicationController
     else
       flash[:alert] = "Did not save!"
     end
-
     redirect_to root_path
+  end
+
+  def destroy
+    @item = current_user.items.find(params[:id])
+    if @item.destroy
+      flash[:notice] = "Item was deleted"
+    else
+      flash.now[:alert] = "Item was not deleted properly"
+    end
+    respond_to do |format|
+      format.html { redirect_to root_path}
+      format.js
+    end
   end
 
 
